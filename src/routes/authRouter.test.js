46ced1f86d9d936995eaa47ca8d104d93e566503
Katie Greer <katieggreer@gmail.com>
2025-01-19 18:05:10 -0700

    register fail test

--- src/routes/authRouter.test.js
+++ src/routes/authRouter.test.js
@@ -1,24 +1,22 @@
 const request = require('supertest');
 const app = require('../service');
+const {Probar} = require("./routeTestFunctions.js");
+const prob = new Probar();
 
 const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
 let testUserAuthToken;
-
-function expectValidJwt(potentialJwt) {
-  expect(potentialJwt).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
-}
 
 beforeAll(async () => {
   testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
   const registerRes = await request(app).post('/api/auth').send(testUser);
   testUserAuthToken = registerRes.body.token;
-  expectValidJwt(testUserAuthToken);
+  prob.expectValidJwt(testUserAuthToken);
 });
 
 test('login', async () => {
   const loginRes = await request(app).put('/api/auth').send(testUser);
   expect(loginRes.status).toBe(200);
-  expectValidJwt(loginRes.body.token);
+  prob.expectValidJwt(loginRes.body.token);
 
   const expectedUser = { ...testUser, roles: [{ role: 'diner' }] };
   delete expectedUser.password;
@@ -26,4 +24,11 @@ test('login', async () => {
 });
 
 //register a user with a franchise role
+
+test("register fail", async () =>{
+  const registerRes = await request(app).post("/api/auth")
+  .set('Content-Type', 'application/json')
+  .send({name: prob.randomName(), email: null, password: prob.randomName()});
+  expect(registerRes.status).toBe(400);
+})
 
